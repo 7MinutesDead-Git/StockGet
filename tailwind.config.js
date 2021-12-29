00099a72536f8d4db929c63c18ab4c78e4f33f0f
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2021-12-28 20:44:55 -0600

    Configure Tailwind CSS with DaisyUI plugin.

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// https://dev.to/akshay03/how-to-setup-tailwind-3-on-rails-7-2dbo
+// If changes aren't updating, run:
+//  npx tailwindcss -i app/assets/stylesheets/application.css -o app/assets/stylesheets/wind.css --watch
+
+module.exports = {
+  // include all HTML, JS and ERB files under the app directory
+  content: ["./app/**/*.{html,js,erb}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      // https://daisyui.com/docs
+      require('daisyui')
+  ],
+
+  daisyui: {
+    themes: [
+        'dark',
+        'synthwave',
+        'lofi',
+        'pastel',
+        'fantasy',
+        'wireframe',
+        'dracula',
+        'light',
+        'cupcake'
+    ]
+  }
+}
