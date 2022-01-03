82ba9995e888f042beff3a4c3f20db0eb877a229
yayorbitgum <50963144+yayorbitgum@users.noreply.github.com>
2022-01-03 01:06:07 -0600

    Alternate method to jQuery in close alert script.

--- app/javascript/close_alert.js
+++ app/javascript/close_alert.js
@@ -1,8 +1,12 @@
-// Add event listener to close alerts when close button is pressed (alert-close class).
-const alert_close = document.querySelectorAll('.alert-close')
 
-alert_close.forEach((x_button) =>
-    x_button.addEventListener('click', function () {
-        x_button.parentElement.classList.toggle('hide-alert')
-    })
-)
+// https://stackoverflow.com/a/17600195/13627106
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Add event listener to close alerts when close button is pressed (alert-close class).
+    const alert_close = document.querySelectorAll('.alert-close')
+
+    alert_close.forEach((x_button) =>
+        x_button.addEventListener('click', function () {
+            x_button.parentElement.classList.toggle('hide-alert')
+        })
+    )
+});
